592a24eec2b22e3acd6e607a2bb57a6eab55f815
matthieuthuetdev <mthuet.pro@gmail.com>
2024-02-16 15:09:28 +0100

    inscription

--- template/sign_up.php
+++ template/sign_up.php
@@ -1,41 +1,54 @@
 <?php
-if (isset($_POST)) {
-    $saisie = $_POST;
-    foreach ($saisie as $input) {
-        $input = trim($input);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = trim($_POST['name']);
+    $first_name = trim($_POST['first_name']);
+    $pseudo_name = trim($_POST['pseudo_name']);
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+    $confirme_password = $_POST['confirme_password'];
+
+    $file = file_get_contents("./../data.json");
+    $data = json_decode($file, true);
+
+    $pseudo_dispo = true;
+    foreach ($data['users'] as $user) {
+        if (isset($user['pseudo_name']) && $user['pseudo_name'] === $pseudo_name) {
+            $pseudo_dispo = false;
+            break;
+        }
     }
-extract($saisie);
-    if (!is_int(strpos($email , "arfp.asso.fr"))) {
-        echo "Ce service est réserver au atudiant du CRM.";
+
+    if (!strpos($email, "arfp.asso.fr")) {
+        echo "Ce service est réservé aux étudiants du CRM.";
     } elseif ($password !== $confirme_password) {
-        echo "Le mot de passe que vous avez saisi et le mot de passe confirmer ne sont pas identique.";
-    } elseif (isset(json_decode("./../data.json",true)["users"][$pseudo_name])) {
-        echo "Le nom d'utilisateur que vous avez saisi est déjà utiliser. essayez en un autre.";
+        echo "Le mot de passe que vous avez saisi et le mot de passe confirmé ne sont pas identiques.";
+    } elseif (!$pseudo_dispo) {
+        echo "Le nom d'utilisateur que vous avez saisi est déjà utilisé. Essayez-en un autre.";
     } else {
-        $data = json_decode(file_get_contents("./../data.json"), true);
-        $users["users"][$pseudo_name] = array(
+        $data['users'][] = array(
             "name" => $name,
             "first_name" => $first_name,
+            "pseudo_name" => $pseudo_name,
             "email" => $email,
             "password" => password_hash($password, PASSWORD_DEFAULT)
         );
-    $json = json_encode($data);
-    file_put_contents("./../data.json", $json);
+        $json = json_encode($data);
+        file_put_contents("./../data.json", $json);
     }
 }
 ?>
 <form action="" method="post">
     <label for="name">Nom</label><br>
-    <input type="text" id="name" Name="name"><br>
+    <input type="text" id="name" name="name" value="<?php echo $name; ?>"><br>
     <label for="first_name">Prénom</label><br>
-    <input type="text" id="first_name" name="first_name"><br>
+    <input type="text" id="first_name" name="first_name" value="<?php echo $first_name; ?>"><br>
     <label for="pseudo_name">Nom d'utilisateur</label><br>
-    <input type="text" id="pseudo_name" name="pseudo_name"><br>
+    <input type="text" id="pseudo_name" name="pseudo_name" value="<?php echo $pseudo_name; ?>"><br>
     <label for="email">Adresse email</label><br>
-    <input type="text" id="email" name="email"><br>
+    <input type="text" id="email" name="email"value <?php echo $email; ?>><br>
     <label for="password">Mot de passe</label><br>
-    <input type="password" id="password" name="password"><br>
+    <input type="password" id="password" name="password"value<?php echo $password; ?>><br>
     <label for="confirme_password">Confirmez le mot de passe</label><br>
-    <input type="password" id="confirme_password" name="confirme_password"><br>
-    <button type="submit">S'inscrir</button>
+    <input type="password" id="confirme_password" name="confirme_password" value="<?php echo $confirme_password; ?>"><br>
+    <button type="submit">S'inscrire</button>
 </form>
--- teste.php
+++ teste.php
@@ -6,9 +6,10 @@ if($contenu === false){
     $users = json_decode($contenu, true);
 }
 
-$users["users"]["Mtbg68"] = array(
+$users["users"][] = array(
     "name" => "Doe",
     "first_name" => "John",
+    "pseudo_name" => "Mtbg685281515",
     "email" => "mattmatt.thuet@arfp.asso.fr",
     "password" => password_hash("JohnDoe123", PASSWORD_DEFAULT)
 );
